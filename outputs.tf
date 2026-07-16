3613745aed8c5745ed8e54d9e92b874cd10b1779
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:52:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "vmware_clusters_id" {
   description = "Map of id values across all vmware_clusters, keyed the same as var.vmware_clusters"
-  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.id }
+  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "vmware_clusters_cluster_node_count" {
   description = "Map of cluster_node_count values across all vmware_clusters, keyed the same as var.vmware_clusters"
-  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.cluster_node_count }
+  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.cluster_node_count if v.cluster_node_count != null }
 }
 output "vmware_clusters_cluster_number" {
   description = "Map of cluster_number values across all vmware_clusters, keyed the same as var.vmware_clusters"
-  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.cluster_number }
+  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.cluster_number if v.cluster_number != null }
 }
 output "vmware_clusters_hosts" {
   description = "Map of hosts values across all vmware_clusters, keyed the same as var.vmware_clusters"
-  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.hosts }
+  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.hosts if v.hosts != null && length(v.hosts) > 0 }
 }
 output "vmware_clusters_name" {
   description = "Map of name values across all vmware_clusters, keyed the same as var.vmware_clusters"
-  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.name }
+  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "vmware_clusters_sku_name" {
   description = "Map of sku_name values across all vmware_clusters, keyed the same as var.vmware_clusters"
-  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.sku_name }
+  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.sku_name if v.sku_name != null && length(v.sku_name) > 0 }
 }
 output "vmware_clusters_vmware_cloud_id" {
   description = "Map of vmware_cloud_id values across all vmware_clusters, keyed the same as var.vmware_clusters"
-  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.vmware_cloud_id }
+  value       = { for k, v in azurerm_vmware_cluster.vmware_clusters : k => v.vmware_cloud_id if v.vmware_cloud_id != null && length(v.vmware_cloud_id) > 0 }
 }
 
